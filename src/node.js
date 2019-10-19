5067805d17a82f210307ddcd00227d31e3726d3c
Admin <kurchinskayavera@gmail.com>
2019-10-19 20:06:01 +0300

    complete node

--- src/node.js
+++ src/node.js
@@ -1,23 +1,67 @@
 class Node {
-	constructor(data, priority) {
+    constructor(data, priority) {
+        this.data = data;
+        this.priority = priority;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
 
-	}
+    }
 
-	appendChild(node) {
+    appendChild(node) {
+        if (this.left === null) {
+            this.left = node;
+            node.parent = this;
+        } else if (this.right === null) {
+            this.right = node;
+            node.parent = this;
+        }
+    }
 
-	}
+    removeChild(node) {
+        if (this.left === node) {
+            this.left = null;
+            node.parent = null;
 
-	removeChild(node) {
+        } else if (this.right === node) {
+            this.right = null;
+            node.parent = null;
+        } else throw Error
+    }
 
-	}
+    remove() {
+        if (this.parent !== null) {
+            this.parent.removeChild(this)
+        }
+    }
 
-	remove() {
+    swapWithParent() {
+        if (this.parent) {
+            if (this.left) this.left.parent = this.parent;
+            if (this.right) this.right.parent = this.parent;
 
-	}
+            if (this === this.parent.left) {
+                if (this.parent.right) this.parent.right.parent = this;
+                [this.parent.left, this.parent.right, this.left, this.right] = [this.left, this.right, this.parent, this.parent.right];
+            }
 
-	swapWithParent() {
-		
-	}
+            if (this === this.parent.right) {
+                if (this.parent.left) this.parent.left.parent = this;
+                [this.parent.left, this.parent.right, this.left, this.right] = [this.left, this.right, this.parent.left, this.parent];
+            }
+
+            if (this.parent.parent) {
+                if (this.parent === this.parent.parent.left) {
+                    this.parent.parent.left = this;
+                }
+                if (this.parent === this.parent.parent.right) {
+                    this.parent.parent.right = this;
+                }
+            }
+            [this.parent.parent, this.parent] = [this, this.parent.parent];
+        }
+    }
+
 }
 
 module.exports = Node;
